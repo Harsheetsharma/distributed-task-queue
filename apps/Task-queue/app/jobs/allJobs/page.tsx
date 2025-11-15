7847e588ef24c47dd39d9ab83458f4701d7296fd
Harsheetsharma <harshitsharma8012@gmail.com>
2025-11-15 17:53:09 +0530

    some styling

--- apps/Task-queue/app/jobs/allJobs/page.tsx
+++ apps/Task-queue/app/jobs/allJobs/page.tsx
@@ -10,6 +10,7 @@ export default function () {
   const jobId = searchParams.get("jobId");
   const Jobid = useRecoilValue(JobsId);
   const [status, setStatus] = useState("");
+  const [type, setType] = useState("");
   console.log(Jobid);
   useEffect(() => {
     if (!Jobid && jobId) return;
@@ -19,6 +20,7 @@ export default function () {
       try {
         const response = await axios.get(`http://localhost:4000/jobs/${jobId}`);
         setStatus(response.data.status);
+        setType(response?.data?.type);
       } catch (err) {
         console.error("Failed to fetch job status:", err);
       }
@@ -40,10 +42,28 @@ export default function () {
 
   return (
     <div>
-      <div className="text-3xl text-black ">all jobs here!</div>
-      <div className=" ms-10 mt-4 h-60 p-4 border-2 items-start rounded shadow-md w-1/4 flex justify-between">
-        <div>Name of job</div>
-        <div>Status {status}</div>
+      <div className="text-2xl sm:text-3xl text-black">all jobs here!</div>
+
+      <div
+        className="ms-0 sm:ms-10 mt-4 h-auto sm:h-60 p-4 
+            border-2 rounded shadow-md 
+            w-full sm:w-1/2 lg:w-1/4"
+      >
+        <div className="flex justify-between w-full text-sm sm:text-base">
+          <div className="ps-2">Job</div>
+          <div className="pe-2">Status</div>
+        </div>
+
+        <div className="h-[0.5px] w-full bg-gray-700 my-2"></div>
+
+        <div className="flex justify-between text-sm sm:text-base">
+          <div>{type.toUpperCase()}</div>
+          <div
+            className={status === "SUCCESS" ? "text-green-600" : "text-red-500"}
+          >
+            {status}
+          </div>
+        </div>
       </div>
     </div>
   );
